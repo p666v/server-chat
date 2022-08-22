8f9ee08d596ed63e5c8731051ae7a5a8fb8f2b04
Pavel Konovalov <konovalovpv.81@gmail.com>
2022-08-22 15:00:54 +0300

    Чат 2. Паттерны проектирования. Паттерн Наблюдатель.

---
+++ src/main/java/ru/itsjava/services/ClientRunnable.java
@@ -0,0 +1,34 @@
+package ru.itsjava.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+@RequiredArgsConstructor
+public class ClientRunnable implements Runnable, Observer {
+    private final Socket socket;
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        System.out.println("Client connection");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String messageFromClient;
+        while ((messageFromClient = bufferedReader.readLine()) != null) {
+            System.out.println(messageFromClient);
+        }
+
+    }
+
+    @SneakyThrows
+    @Override
+    public void notifyMe(String message) {
+        PrintWriter clientWriter = new PrintWriter(socket.getOutputStream());
+        clientWriter.println(message);
+        clientWriter.flush();
+    }
+}
---
+++ src/main/java/ru/itsjava/services/Observer.java
@@ -0,0 +1,5 @@
+package ru.itsjava.services;
+
+public interface Observer {
+    void notifyMe(String message);
+}
